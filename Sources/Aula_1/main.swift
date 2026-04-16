fee65e84d09df0ce3dd15a9d17e1ee6757c18e14
Gabriel Mello <120739225+guts7101997@users.noreply.github.com>
2026-04-16 20:23:52 +0000

    Alterações feitas da aula 1

--- Sources/Aula_1/main.swift
+++ Sources/Aula_1/main.swift
@@ -1,3 +1,25 @@
 import Foundation
 
-print("Aula 1 de Swift!!")
+//arrays
+var array: [Int] = [1,2,3,4,5]
+array.append(3)
+array.remove(at: 2)
+print(array)
+
+//sets
+
+var set : Set<String> = ["açao", "ficcao-cientifica"]
+set.insert("terror")
+print(set)
+
+var set2 : Set<String> = ["ficcao-cientifica"]
+print(set.intersection(set2))
+
+//tuplas 
+var tupla = (lugar: "Mcdonalds", valor: 30)
+print(tupla.valor)
+
+//dicionario
+var dicionario : [String : Int] = ["X": 1, "Y": 2]
+dicionario["B"] = 4
+print(dicionario["X", default: 0])
